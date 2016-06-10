5ae39650572bf6a262442f6835d5d28eecb5a1e4
Atze van der Ploeg <atzeus@gmail.com>
2016-06-10 13:10:46 +0200

    Arrows

--- Arrow.hs
+++ Arrow.hs
@@ -12,16 +12,15 @@ import Data.Monoid
 newtype Cage s x = Cage { open :: KeyMap s -> x }
   deriving (Functor, Applicative)
 
-newtype ArrowSyn a s x =
+newtype ArrowSyntax a s x =
     AS (WriterT (EnvArrow a s) (KeyM s) x)
        deriving (Functor,Applicative,Monad)
-type EnvArrow a s = EndoA a (KeyMap s)
 
-newtype EndoA a x = EndoA (a x x)
+newtype EnvArrow a s = EnvArrow (a (KeyMap s) (KeyMap s))
 
-instance Arrow a => Monoid (EndoA a x) where
-    mempty = EndoA (arr id)
-    mappend (EndoA l) (EndoA r) = EndoA (l >>> r)
+instance Arrow a => Monoid (EnvArrow a x) where
+    mempty = EnvArrow (arr id)
+    mappend (EnvArrow l) (EnvArrow r) = EnvArrow (l >>> r)
 
 toCage :: Key s a -> Cage s a
 toCage k = Cage (\env -> env ! k)
@@ -44,12 +43,12 @@ withEnv c = dup >>> first (elimEnv c)
 toEnvArrow ::  Arrow a =>  
            Cage s x  -> Key s y   -> 
            a x y -> EnvArrow a s
-toEnvArrow inC outK a  = EndoA $ 
+toEnvArrow inC outK a  = EnvArrow $ 
    withEnv inC >>> first a >>> extendEnv outK
 
 (-<) :: Arrow a =>
         a x y ->
-        (Cage s x -> ArrowSyn a s (Cage s y))
+        (Cage s x -> ArrowSyntax a s (Cage s y))
 a -< inC = AS $
    do  outK <- lift newKey
        tell (toEnvArrow inC outK a)
@@ -58,11 +57,11 @@ a -< inC = AS $
 fromEnvArrow ::  Arrow a =>
              Key s x   -> Cage s y  ->
              EnvArrow a s -> a x y
-fromEnvArrow inK outC (EndoA a)  =
+fromEnvArrow inK outC (EnvArrow a)  =
    introEnv inK >>> a >>> elimEnv outC
 
 proc ::  Arrow a =>
-             (forall s. Cage s x -> ArrowSyn a s (Cage s y)) ->
+             (forall s. Cage s x -> ArrowSyntax a s (Cage s y)) ->
              a x y
 proc f = runKeyM $
       do  inK <- newKey
@@ -75,12 +74,12 @@ makeLoopable k c a = flip >>> extendEnv k >>> a >>> withEnv c >>> flip
   where flip = arr (\(x,y) -> (y,x))
 
 
-afix :: ArrowLoop a => (Cage s x -> ArrowSyn a s (Cage s x)) -> ArrowSyn a s (Cage s x)
+afix :: ArrowLoop a => (Cage s x -> ArrowSyntax a s (Cage s x)) -> ArrowSyntax a s (Cage s x)
 afix f = AS $
   do k <- lift newKey
      let AS m = f (toCage k)
-     (outC, EndoA a) <- lift $ runWriterT m
-     tell (EndoA $ loop (makeLoopable k outC a))
+     (outC, EnvArrow a) <- lift $ runWriterT m
+     tell (EnvArrow $ loop (makeLoopable k outC a))
      return (toCage k)
    
 
@@ -102,42 +101,43 @@ instance (Functor m, Applicative m, Monad m) => RelativeMonad (IdF m) Id where
 
 -- every relative monad is a monad via the following trick: (Svenningsson and Svensson), who did not 
 -- formulate it as such
-data RmSyn rm v a where
-   Pure :: a -> RmSyn rm v a
-   Unpure :: rm a -> (v a -> RmSyn rm v b) -> RmSyn rm v b
+data RelativeMSyntax rm v a where
+   Pure :: a -> RelativeMSyntax rm v a
+   Unpure ::  rm a -> (v a -> RelativeMSyntax rm v b) 
+              -> RelativeMSyntax rm v b
 
-instance Monad (RmSyn rm v) where
+instance Monad (RelativeMSyntax rm v) where
   return = Pure
   (Pure x) >>= f = f x
   (Unpure m f) >>= g = Unpure m (\x -> f x >>= g)
 
-fromRm :: RelativeMonad rm v => rm a -> RmSyn rm v (v a)
-fromRm m = Unpure m (return)
+fromRelativeM :: RelativeMonad rm v => 
+   rm a -> RelativeMSyntax rm v (v a)
+fromRelativeM m = Unpure m return
 
-toRm :: RelativeMonad rm v => RmSyn rm v (v a) -> rm a
-toRm (Pure x) = rreturn x
-toRm (Unpure m f) = m .>>= (toRm . f)
+toRelativeM :: RelativeMonad rm v =>
+                  RelativeMSyntax rm v (v a) -> rm a
+toRelativeM (Pure x)      = rreturn x
+toRelativeM (Unpure m f)  = m .>>= (toRelativeM . f)
  
-{-
 
-instance RelativeMonad rm v => Monad (RmSyn rm v) where
-  return = Pure
-  (Pure x) >>= f = f x
-  (Unpure m) >>= f = 
-
-  
+-- not really a monad, but up to observation
 
 data RmSyn rm v a where
   Ret  :: a -> RmSyn rm v a
   Lift :: rm a -> RmSyn rm v (v a)
   Bind :: RmSyn rm v a -> (a -> RmSyn rm v b) -> RmSyn rm v b
 
+instance Monad (RmSyn rm v) where
+  return = Ret
+  (>>=)  = Bind
+
 desugar :: RelativeMonad rm v => RmSyn rm v (v a) -> rm a
 desugar (Ret x) = rreturn x
 -- remove pure computations
 desugar (Bind (Ret x) f) = desugar (f x)
 desugar (Bind (Lift m) f) = m .>>= (desugar . f)
--}
+
 
 class RelativeMonad m v => RelativeMonadFix m v where
   rmfix :: (v a -> m a) -> m a
@@ -150,7 +150,7 @@ class RelativeMonad m v => RelativeMonadFix m v where
     -- nesting : rmfix (\x -> rmfix (\y -> f x y)) = rmfix (\x -> f x x)
 
 -- arrow relative monad
-data Arm a s x = Arm { getArm :: ArrowSyn a s (Cage s x) }
+data Arm a s x = Arm { getArm :: ArrowSyntax a s (Cage s x) }
 
 instance Arrow a => RelativeMonad (Arm a s) (Cage s) where
   rreturn x = Arm (return x)
